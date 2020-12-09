cb6ef5a6adfc1a4d73f389a2ff8d000658ea10c0
RemiWLeBlanc <remiwleblanc@gmail.colm>
2020-12-09 13:46:27 -0800

    Clean up.

---
+++ .ipynb_checkpoints/MontyHallSimulation-checkpoint.py
@@ -0,0 +1,98 @@
+# Remi LeBlanc
+# Monty Hall Simulation
+
+import random
+
+
+def monty_hall(interactive=True, switch=None):
+    doors = [False, False, False]  # false is a zonk
+    car = random.randrange(3)
+    doors[car] = True  # car
+
+    if interactive:
+        while True:  # make sure user enters 1 2 or 3
+            choice = input("Pick door 1, 2, or 3: ")
+            if choice in {'1', '2', '3'}:
+                break
+        choice = int(choice) - 1  # for indexing
+    else:
+        choice = random.randrange(3)
+
+    a = {0, 1, 2}
+    b = {choice, car}
+    reveal = a.difference(b)  # set of doors not chosen and not with the car, could be two doors
+    if len(reveal) > 1:
+        reveal = random.sample(reveal, 1)  # if they chose the right door, randomly choose to open one of the others
+    reveal = reveal.pop()  # get the door from the single item set
+    c = {choice, reveal}
+    other = a.difference(c).pop()  # gets the door other than their choice and one revealed
+
+    if interactive:
+        print(f"Door {reveal+1} does not have the car.")
+        while True:  # make sure user enters y or n
+            switch = input(f"You have chosen door {choice+1}. Would you like to switch to door {other+1}? (y/n)")
+            if switch == 'y':
+                choice = other
+                break
+            elif switch == 'n':
+                break
+        if doors[choice]:
+            print(f"Door {choice+1} has the car! Congratulations!\n")
+        else:
+            print(f"Door {choice+1} is zonk. Door {car+1} had the car! Better luck next time!\n")
+
+    else:  # not interactive
+        if switch:
+            choice = other  # switch choice
+            return doors[choice]
+        else:
+            return doors[choice]
+
+
+def simulation(switch):
+    wins = 0
+
+    while True:  # make sure user enters a number
+        n = input("Enter the number of simulations you wish to run: ")
+        try:
+            n = int(n)
+            break
+        except ValueError:
+            pass
+
+    for i in range(n):
+        if monty_hall(False, switch):  # not interactive and switch based on arg
+            wins += 1
+
+    winning_percent = round((wins/n)*100, 2)
+    if switch:
+        action = "Switching"
+    else:
+        action = "Staying"
+    print(f"{action} every time gave a winning percentage of {winning_percent}%")
+
+
+if __name__ == '__main__':
+    while True:
+        option = input("""
+Monty Hall Game!
+Press 1 for interactive game
+Press 2 to simulate switching every time
+Press 3 to simulate staying every time
+""")
+
+        if option == '1':
+            monty_hall()
+        elif option == '2':
+            simulation(switch=True)
+        elif option == '3':
+            simulation(switch=False)
+
+        while True:  # make sure user enters y or n
+            ans = input("Play again? (y/n)")
+            if ans == 'y' or ans == 'n':
+                break
+        if ans == 'n':
+             break
+
+    print('Thanks for playing!')
--- MontyHallSimulation.py
+++ MontyHallSimulation.py
@@ -5,94 +5,115 @@ import random
 
 
 def monty_hall(interactive=True, switch=None):
+    """
+    Runs a single monty hall problem, either interactive or not interactive version, based on parameters.
+    The non interactive version takes an additional parameter to switch or stay on the simulation.
+
+    :param interactive: Boolean. True = interactive version, False = random simulation
+    :param switch: Boolean. For the non interactive version: switch = True, always switch. switch = False, always stay
+    :return: Non interactive version returns True or False based on if correct door was selected (correct -> True)
+    """
+
     doors = [False, False, False]  # false is a zonk
-    car = random.randrange(3)
+    car = random.randrange(3)  # randomly pick a door for car to be behind
     doors[car] = True  # car
 
     if interactive:
+        # user picks a door
         while True:  # make sure user enters 1 2 or 3
             choice = input("Pick door 1, 2, or 3: ")
             if choice in {'1', '2', '3'}:
                 break
         choice = int(choice) - 1  # for indexing
-    else:
+    else:  # Simulation
+        # randomly choose a door
         choice = random.randrange(3)
 
-    a = {0, 1, 2}
-    b = {choice, car}
-    reveal = a.difference(b)  # set of doors not chosen and not with the car, could be two doors
-    if len(reveal) > 1:
-        reveal = random.sample(reveal, 1)  # if they chose the right door, randomly choose to open one of the others
+    # calculate which door to reveal
+    all_doors = {0, 1, 2}
+    dont_open = {choice, car}
+    reveal = all_doors.difference(dont_open)  # set of doors not chosen and not with the car, could be one or two doors
+    if len(reveal) > 1:  # if they chose the right door
+        reveal = random.sample(reveal, 1)  # randomly choose to open one of the other two doors
     reveal = reveal.pop()  # get the door from the single item set
-    c = {choice, reveal}
-    other = a.difference(c).pop()  # gets the door other than their choice and one revealed
+
+    # get the door other than their choice and one revealed, for the switch option
+    other_door = all_doors.difference({choice, reveal}).pop()
 
     if interactive:
         print(f"Door {reveal+1} does not have the car.")
         while True:  # make sure user enters y or n
-            switch = input(f"You have chosen door {choice+1}. Would you like to switch to door {other+1}? (y/n)")
+            switch = input(f"You have chosen door {choice+1}. Would you like to switch to door {other_door+1}? (y/n)")
             if switch == 'y':
-                choice = other
+                choice = other_door
                 break
             elif switch == 'n':
                 break
+
+        # print the results of the game
         if doors[choice]:
             print(f"Door {choice+1} has the car! Congratulations!\n")
         else:
             print(f"Door {choice+1} is zonk. Door {car+1} had the car! Better luck next time!\n")
 
     else:  # not interactive
-        if switch:
-            choice = other  # switch choice
-            return doors[choice]
-        else:
-            return doors[choice]
+        if switch:  # parameter passed into function call
+            choice = other_door  # switch first choice
+
+        return doors[choice]  # True or False
 
 
-def simulation(switch):
+def simulation(switch, n):
+    """
+    Calls monty_hall() n times either switching or staying every time, based on boolean parameter switch.
+
+    :param switch: True = always switch. False = always stay.
+    :param n: Number of times to run random simulation.
+    :return: prints out the winning percentage from n simulations of the game
+    """
+
     wins = 0
-
-    while True:  # make sure user enters a number
-        n = input("Enter the number of simulations you wish to run: ")
-        try:
-            n = int(n)
-            break
-        except ValueError:
-            pass
-
     for i in range(n):
-        if monty_hall(False, switch):  # not interactive and switch based on arg
+        if monty_hall(False, switch):  # not interactive and switch based on parameter
             wins += 1
 
     winning_percent = round((wins/n)*100, 2)
-    if switch:
-        action = "Switching"
-    else:
-        action = "Staying"
-    print(f"{action} every time gave a winning percentage of {winning_percent}%")
+
+    print(f"{['Switching' if switch else 'Staying'][0]} every time gave a winning percentage of {winning_percent}% after {n} random simulations.")
 
 
 if __name__ == '__main__':
     while True:
         option = input("""
 Monty Hall Game!
-Press 1 for interactive game
-Press 2 to simulate switching every time
-Press 3 to simulate staying every time
+Enter 1 for interactive game
+Enter 2 to simulate switching every time
+Enter 3 to simulate staying every time
 """)
 
         if option == '1':
             monty_hall()
-        elif option == '2':
-            simulation(switch=True)
-        elif option == '3':
-            simulation(switch=False)
+
+        else:  # Computer simulation
+            # ask how many times user wants simulation ran
+            while True:  # make sure user enters a number
+                n = input("Enter the number of simulations you wish to run: ")
+                try:
+                    n = int(n)
+                    break
+                except ValueError:  # not a number, ask again
+                    pass
+
+            if option == '2':  # switch
+                simulation(True, n)
+            elif option == '3':  # stay
+                simulation(False, n)
 
         while True:  # make sure user enters y or n
             ans = input("Play again? (y/n)")
             if ans == 'y' or ans == 'n':
                 break
-        if ans == 'n':
-             break
+        if ans == 'n':  # end game
+            break
 
     print('Thanks for playing!')
